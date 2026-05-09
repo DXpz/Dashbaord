497c3ef88067581f08a66d891d9088eaa31d225b
DXpz <antoniohector413@gmail.com>
2026-05-09 12:24:44 -0600

    Increase access_token cookie maxAge to 7 days

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: Request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 3600,
+      maxAge: 60 * 60 * 24 * 7,
       path: '/',
     });
 
