fdac032fc2f6d118115a28e8d41d6ceceb0156a2
DXpz <antoniohector413@gmail.com>
2026-05-09 14:11:56 -0600

    Remove rewrites from next.config - using /api/proxy route directly

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    { source: '/api/:path*', destination: '/api/proxy?_path=:path*' },
-  ],
 };
 
 module.exports = nextConfig;
